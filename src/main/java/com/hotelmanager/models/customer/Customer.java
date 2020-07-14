1af19299e4dae000a8d656cc6c28164c283ebae0
bangmac <anhbexd@gmail.com>
2020-07-14 09:46:06 +0700

    clean

--- src/main/java/com/hotelmanager/models/customer/Customer.java
+++ src/main/java/com/hotelmanager/models/customer/Customer.java
@@ -1,25 +1,4 @@
 package com.hotelmanager.models.customer;
 
-import lombok.Data;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-@Data
 public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private String address;
-    private String idNumber;
-    private String email;
-    private String userName;
-    private String password;
-    private String company;
 }
