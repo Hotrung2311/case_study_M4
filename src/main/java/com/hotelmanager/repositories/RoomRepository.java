25e8a12308a511e7ca4b98782b9ebf7bdfeb5680
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-07-14 15:39:34 +0700

    sua lai repo

--- src/main/java/com/hotelmanager/repositories/RoomRepository.java
+++ src/main/java/com/hotelmanager/repositories/RoomRepository.java
@@ -1,11 +1,8 @@
 package com.hotelmanager.repositories;
 
-<<<<<<< HEAD
 import com.hotelmanager.models.room.Room;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoomRepository extends CrudRepository<Room, Long> {
-=======
-public interface RoomRepository {
->>>>>>> 6bccdae96905c03a573de0621cde44902735f68f
+
 }
